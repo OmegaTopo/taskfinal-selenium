ac167afd38c852266e06f777eee170eea3ada275
Giovannelm <giovanne.lopes.menicheli@gmail.com>
2023-03-19 18:29:32 -0300

    Finalização testes forms

--- captacao-vemser/src/test/java/captacao/vemser/dbccompany/pages/FormPage.java
+++ captacao-vemser/src/test/java/captacao/vemser/dbccompany/pages/FormPage.java
@@ -2,7 +2,9 @@ package captacao.vemser.dbccompany.pages;
 
 import net.datafaker.Faker;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
+import java.security.Key;
 import java.util.Locale;
 
 public class FormPage extends BasePage{
@@ -20,6 +22,7 @@ public class FormPage extends BasePage{
     private static final By btnEspanholAvancado = By.cssSelector("#menu-espanhol li:nth-child(4)");
     private static final By btnEspanholFluente = By.cssSelector("#menu-espanhol li:nth-child(5)");
     private static final By btnLinguagemInteresse = By.cssSelector("div:nth-child(9) > div > div");
+    private static final By btnLinguagemInteresseSair = By.cssSelector("#menu- > div.MuiBackdrop-root.MuiBackdrop-invisible.css-esi9ax");
     private static final By btnCSharp = By.cssSelector("#menu- li:nth-child(1) .MuiButtonBase-root");
     private static final By btnJavaScript = By.cssSelector("#menu- li:nth-child(2) .MuiButtonBase-root");
     private static final By btnKotlin = By.cssSelector("#menu- li:nth-child(3) .MuiButtonBase-root");
@@ -35,11 +38,17 @@ public class FormPage extends BasePage{
     private static final By btnDisponibilidadeEfetivacaoSim = By.cssSelector("#s2-candidato-efetivacao-sim .css-hyxlzm");
     private static final By btnDisponobilidadeEfetivacaoNao = By.cssSelector("#s2-candidato-efetivacao-nao .css-hyxlzm");
     private static final By campoLinkLinkedIn = By.cssSelector("[id=s2-candidato-linkedin]");
-    private static final By btnConcordarDados = By.cssSelector("#s2-candidato-lgpd path");
+    private static final By campoLabelLinkedIn = By.cssSelector("#s2-candidato-linkedin-label");
+    private static final By btnConcordarDados = By.cssSelector("#s2-candidato-lgpd span:nth-child(1)");
     private static final By btnIrParaInscricao = By.cssSelector("div > button");
     private static final By btnEnviar = By.cssSelector("[id=s2-botao-submit]");
     private static final By campoErroInstituicao = By.cssSelector("#instituicao-de-ensino-candidato-helper-text");
     private static final By campoLabelEspanhol = By.cssSelector("div:nth-child(6) > div > label");
+    private static final By campoErroLinguagem = By.cssSelector("div:nth-child(9) > div > p");
+    private static final By campoTextoFormulario = By.cssSelector("div:nth-child(3) > span > span.MuiStepLabel-labelContainer.css-h2cmlr > span > div > p");
+    private static final By campoErroMotivo = By.cssSelector("#s2-candidato-motivo-helper-text");
+    private static final By campoPaginaAtual = By.cssSelector("form > div:nth-child(1) > label");
+    private static final By campoErroConcordar = By.cssSelector("#mensagem-erro-lgpd");
 
     public void marcarTurnoNoite(){
         clicar(campoTurnoNoite);
@@ -59,16 +68,65 @@ public class FormPage extends BasePage{
     public void clicarEspanhol(){
         clicar(btnSelecaoEspanhol);
     }
-
     public void clicarEspanholNenhum(){
         clicar(btnEspanholNenhum);
     }
-
     public void clicarEspanholFluente(){
         clicar(btnEspanholFluente);
     }
     public String corCaixaEspanhol(){
         return extraiCores(campoLabelEspanhol);
+    }
+    public void clicarBtnLinguagemInteresse() {
+        clicar(btnLinguagemInteresse);
+    }
+    public void clicarBtnLinguagemInteresseSair(){
+        clicar(btnLinguagemInteresseSair);
+    }
+    public void clicarEmJava() {
+        clicar(btnJava);
+    }
+    public String verificaTextoErroLinguagem(){
+        return extraiTexto(campoErroLinguagem);
+    }
+    public void preencheCampoMotivo(){
+        preencheCampo(campoMotivoInteresse, faker.lorem().sentence(20));
+    }
+    public void preencheCampoMotivoInsuficiente(){
+        preencheCampo(campoMotivoInteresse, "abcdefghi");
+    }
+    public void preencheCampoMotivoInvalido(){
+        preencheCampo(campoMotivoInteresse, "98734^!#$253462$%");
+    }
+    public Boolean verificaExistenciaCampoErroMotivo(){
+        return verificaExistenciaElemento(campoErroMotivo);
+    }
+    public String extraiTextoCampoErroMotivo(){
+        return extraiTexto(campoErroMotivo);
+    }
+    public void preencheCampoLinkedIn(){
+        preencheCampo(campoLinkLinkedIn, "https://www.linkedin.com/in/giovanne-lopes-16289b215/");
+    }
+    public void preencheCampoLinkedInOutroSite(){
+        preencheCampo(campoLinkLinkedIn, "https://google.com/");
+    }
+    public void preencheCampoLinkedInInvalido(){
+        preencheCampo(campoLinkLinkedIn, "nfq39784fgnoia");
+    }
+    public Boolean verificaPaginaAtual(){
+        return verificaExistenciaElemento(campoPaginaAtual);
+    }
+    public String extrairClasseAvisoLinkedIn(){
+        return extraiClasses(campoLabelLinkedIn);
+    }
+    public void clicarConcordarTratamento(){
+        clicar(btnConcordarDados);
+    }
+    public String verificaTextoErroConcordar(){
+        return extraiTexto(campoErroConcordar);
+    }
+    public void clicarBtnVoltar(){
+        clicar(btnIrParaInscricao);
     }
 
 }
--- captacao-vemser/src/test/java/captacao/vemser/dbccompany/steps/FormSteps.java
+++ captacao-vemser/src/test/java/captacao/vemser/dbccompany/steps/FormSteps.java
@@ -74,6 +74,111 @@ public class FormSteps extends BaseSteps{
         Assert.assertNotEquals(corLabelEspanholAntes, corLabelEspanholDepois);
     }
 
+    @Test
+    public void selecionarUmaLinguagemProgramacao(){
+        formPage.clicarBtnLinguagemInteresse();
+        formPage.clicarEmJava();
+        formPage.clicarBtnLinguagemInteresseSair();
+        formPage.clicarEnviar();
 
+        String validador = formPage.verificaTextoErroLinguagem();
+        Assert.assertEquals("", validador);
+    }
 
+    @Test
+    public void deixarLinguagememBranco(){
+        formPage.clicarEnviar();
+
+        String validador = formPage.verificaTextoErroLinguagem();
+        Assert.assertEquals("Pelo menos uma linguagem deve ser selecionada", validador);
+    }
+
+    @Test
+    public void preencherMotivoInteresse(){
+        formPage.preencheCampoMotivo();
+        formPage.clicarEnviar();
+
+        Boolean campoExiste = formPage.verificaExistenciaCampoErroMotivo();
+        Assert.assertFalse(campoExiste);
+    }
+
+    @Test
+    public void preencherMotivoInteresseInsuficiente(){
+        formPage.preencheCampoMotivoInsuficiente();
+        formPage.clicarEnviar();
+
+        String validador = formPage.extraiTextoCampoErroMotivo();
+        Assert.assertEquals("São necessários 10 caracteres, no mínimo", validador);
+    }
+
+    @Test
+    public void preencherMotivoInteresseInvalido(){
+        formPage.preencheCampoMotivoInvalido();
+        formPage.clicarEnviar();
+
+        String validador = formPage.extraiTextoCampoErroMotivo();
+        Assert.assertEquals("A resposta deve ter apenas letras e espaços", validador);
+    }
+
+    @Test
+    public void deixarMotivoInteresseEmBranco(){
+        formPage.clicarEnviar();
+
+        String validador = formPage.extraiTextoCampoErroMotivo();
+        Assert.assertEquals("Campo obrigatório", validador);
+    }
+
+    @Test
+    public void preencherLinkedIn(){
+        String classeErroLinkAntes = formPage.extrairClasseAvisoLinkedIn();
+        formPage.preencheCampoLinkedIn();
+        formPage.clicarEnviar();
+
+        String classeErroLinkDepois = formPage.extrairClasseAvisoLinkedIn();
+        Assert.assertNotEquals(classeErroLinkAntes, classeErroLinkDepois);
+    }
+
+    @Test
+    public void preencherLinkedInInvalido(){
+        formPage.preencheCampoLinkedInInvalido();
+        formPage.clicarEnviar();
+
+        Boolean campoExiste = formPage.verificaPaginaAtual();
+        Assert.assertTrue(campoExiste);
+    }
+
+    @Test
+    public void preencherLinkedInOutroLink(){
+        String classeErroLinkAntes = formPage.extrairClasseAvisoLinkedIn();
+        formPage.preencheCampoLinkedInOutroSite();
+        formPage.clicarEnviar();
+
+        String classeErroLinkDepois = formPage.extrairClasseAvisoLinkedIn();
+        Assert.assertNotEquals(classeErroLinkAntes, classeErroLinkDepois);
+    }
+
+    @Test
+    public void concordarTratamento(){
+        formPage.clicarConcordarTratamento();
+        formPage.clicarEnviar();
+
+        String validador = formPage.verificaTextoErroConcordar();
+        Assert.assertEquals("", validador);
+    }
+
+    @Test
+    public void naoConcordarTratamento(){
+        formPage.clicarEnviar();
+
+        String validador = formPage.verificaTextoErroConcordar();
+        Assert.assertEquals("É necessário aceitar os termos para prosseguir", validador);
+    }
+
+    @Test
+    public void clicarBotaoVoltar(){
+        formPage.clicarBtnVoltar();
+
+        Boolean paginaForm = formPage.verificaPaginaAtual();
+        Assert.assertFalse(paginaForm);
+    }
 }
