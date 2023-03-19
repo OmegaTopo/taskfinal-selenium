2e83d033fefc90b7efcf0622eec75f4d124aae96
OmegaTopo <74385000+OmegaTopo@users.noreply.github.com>
2023-03-18 21:18:16 -0300

    FC01-FC04-FC11 - Criação de testes de nome, CPF e estado

--- captacao-vemser/src/test/java/captacao/vemser/dbccompany/pages/InfoPage.java
+++ captacao-vemser/src/test/java/captacao/vemser/dbccompany/pages/InfoPage.java
@@ -19,15 +19,28 @@ public class InfoPage extends BasePage {
     private static final By campoDataDeNascimento = By.cssSelector("[id=step-1-dataNascimento]");
     private static final By campoCidade = By.cssSelector("[id=step-1-cidade]");
     private static final By campoEstado = By.cssSelector("[id=step-1-estado]");
+    private static final By campoEstadoPA = By.cssSelector("li:nth-child(14)");
     private static final By campoNeurodiversidade = By.cssSelector("div:nth-child(9) > div > div > svg");
     private static final By btnProximo = By.cssSelector("[id=step-1-enviar]");
     private static final By campoErroRG = By.cssSelector("p[id=\"step-1-rg-helper-text\"]");
+    private static final By campoErroNome = By.cssSelector("#step-1-nome-helper-text");
+    private static final By campoErroCPF = By.cssSelector("#step-1-cpf-helper-text");
     private static final By opcaoNeurodiversidadeNao = By.cssSelector("#menu-neurodiversidadeBoolean li:nth-child(1)");
 
-
     public void preencherNomeValido() {
-
         preencheCampo(campoNome, faker.name().nameWithMiddle());
+    }
+    public void preencherNomeSemSobrenome() {
+        preencheCampo(campoNome, "Nome");
+    }
+    public void preencherNomeComNumero() {
+        preencheCampo(campoNome, "123456789");
+    }
+    public void preencherNomeCurto() {
+        preencheCampo(campoNome, "a");
+    }
+    public void preencherNomeEmBranco() {
+        preencheCampo(campoNome, "");
     }
 
     public void preencherEmailValido() {
@@ -48,9 +61,16 @@ public class InfoPage extends BasePage {
     }
 
     public void preencherCPFValido() {
-
         clicar(campoCPF);
         preencheCampo(campoCPF, faker.cpf().toString());
+    }
+    public void preencherCPFInvalido() {
+        clicar(campoCPF);
+        preencheCampo(campoCPF, faker.cpf().invalid());
+    }
+    public void preencherCPFCurto(){
+        clicar(campoCPF);
+        preencheCampo(campoCPF, "123");
     }
 
     public void preencherCampoCelularValido() {
@@ -69,12 +89,10 @@ public class InfoPage extends BasePage {
         preencheCampo(campoCidade, cidadeValida);
     }
 
-    public void preencherCampoEstadoValido() {
-        String estadoValido = "Pará";
-
-        preencheCampo(campoEstado, estadoValido);
+    public void selecionarEstadoPA() {
+        clicar(campoEstado);
+        clicar(campoEstadoPA);
     }
-
     public void preencherCampoNeurodiversidade() {
 
         clicar(campoNeurodiversidade);
@@ -82,12 +100,19 @@ public class InfoPage extends BasePage {
     }
 
     public void clicarEmProximo() {
-
         clicar(btnProximo);
     }
 
     public Boolean verificaExistenciaCampoErroRG() {
 
         return verificaExistenciaElemento(campoErroRG);
+    }
+    public String verificaCampoErroNome(){
+        String texto = extraiTexto(campoErroNome);
+        return texto;
+    }
+    public String verificaCampoErroCPF(){
+        String texto = extraiTexto(campoErroCPF);
+        return texto;
     }
 }
